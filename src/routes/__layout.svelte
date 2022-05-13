807ec4e87b325d2dd68495c4624d1419df8570e0
Martin Krizan <mnohosten@gmail.com>
2022-05-13 10:03:20 +0200

    Basic svelte app

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,43 @@
+<div class='container'>
+	<slot></slot>
+</div>
+
+<style>
+  @font-face {
+    font-family: "JetBrains Mono";
+    src: url('/fonts/mono/JetBrainsMono-Regular.woff2');
+  }
+	:global(:root) {
+		--color-bg: #282828;
+		--color-bg0_h: #1d2021;
+		--color-fg: #ebdbb2;
+		--color-red: #cc241d;
+		--color-green: #98971a;
+		--color-yellow: #d79921;
+		--color-blue: #458588;
+		--color-purple: #b16286;
+		--color-aqua: #689d6a;
+		--color-gray: #a89984;
+	}
+	:global(body, h1, h2, h3, h4) {
+		margin: 0;
+		padding: 0;
+	}
+  :global(body) {
+    background-color: var(--color-bg0_h);
+		color: var(--color-fg);
+		text-align: center;
+  }
+	:global(body, input, textarea, select) {
+    font-family: "JetBrains Mono", "Ubuntu Mono", Monospace;
+	}
+
+  .container {
+		text-align: left;
+		margin: auto;
+		padding: 1rem;
+		width: 100%;
+    max-width: 800px;
+		background-color: var(--color-bg);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let userInput = '';
+	let history = [];
+	const runCommand = () => {
+		const [command, ...args] = userInput.split(' ');
+		console.log(command, args);
+		history = [userInput, ...history].slice(0, 3);
+		userInput = '';
+	}
+</script>
+
+<div id='output'>
+	<div id='welcome'>
+		<h1>Hi, I'm <span style='color: var(--color-yellow)'>Martin</span></h1>
+		<p>Welcome to my homepage.</p>
+	</div>
+	<div class='history'>
+		{#each history as item}{item}<br>{/each}
+	</div>
+</div>
+<div id='input-line'>
+	<span class='arrow'>➜</span>
+	<input on:change={runCommand} bind:value={userInput} type='text' autofocus placeholder='help'>
+</div>
+
+<style>
+  input {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    line-height: 2rem;
+    background: none;
+    border: 0;
+    border-radius: 0;
+    color: var(--color-fg);
+    font-size: 1rem;
+    caret-shape: underscore!important;
+    outline: none;
+  }
+  input:focus {
+    border: none;
+  }
+	#welcome {
+		text-align: center;
+	}
+	#input-line {
+		margin-top: 1rem;
+		display: flex;
+		justify-content: center;
+	}
+	.arrow {
+		color: var(--color-aqua);
+    line-height: 2rem;
+		margin-right: 0.5rem;
+	}
+</style>
